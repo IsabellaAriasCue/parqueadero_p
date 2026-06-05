14d1c3fe35d424a0ecef74d577338603ef9bea64
IsabellaAriasCue <iarias579@cue.edu.co>
2026-06-05 16:19:25 -0500

    refactor: extraer constantes de negocio

--- src/parkinguv/tarifas.py
+++ src/parkinguv/tarifas.py
@@ -1,18 +1,23 @@
 import math
 
+MINUTOS_GRATIS = 30
+TARIFA_HORA = 500
 TOPE_DIARIO = 12000
+DESCUENTO_VIP = 0.20
 
 
 def calcular_tarifa(minutos, vip=False):
 
-    if minutos <= 30:
+    if minutos <= MINUTOS_GRATIS:
         return 0
 
-    horas = math.ceil((minutos - 30) / 60)
+    horas = math.ceil(
+        (minutos - MINUTOS_GRATIS) / 60
+    )
 
-    total = horas * 500
+    total = horas * TARIFA_HORA
 
     if vip:
-        total *= 0.8
+        total *= (1 - DESCUENTO_VIP)
 
     return min(total, TOPE_DIARIO)
