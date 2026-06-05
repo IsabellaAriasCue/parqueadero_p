73d9f78beac62c468c028bdd0666508fb1a57607
IsabellaAriasCue <iarias579@cue.edu.co>
2026-06-05 16:18:37 -0500

    feat: aplicar descuento vip

--- src/parkinguv/tarifas.py
+++ src/parkinguv/tarifas.py
@@ -3,7 +3,7 @@ import math
 TOPE_DIARIO = 12000
 
 
-def calcular_tarifa(minutos):
+def calcular_tarifa(minutos, vip=False):
 
     if minutos <= 30:
         return 0
@@ -11,5 +11,8 @@ def calcular_tarifa(minutos):
     horas = math.ceil((minutos - 30) / 60)
 
     total = horas * 500
+
+    if vip:
+        total *= 0.8
 
     return min(total, TOPE_DIARIO)
